3c19940fdda55251a5f9b83e5570034ff366c333
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-10-13 12:32:11 +0600

    Changed latest to tag number.

--- Formula/resfetch.rb
+++ Formula/resfetch.rb
@@ -1,7 +1,7 @@
 class Resfetch < Formula
     desc "A fast and minimal alternative to neofetch"
     homepage "https://github.com/furtidev/resfetch"
-    url "https://github.com/furtidev/resfetch/releases/latest/download/resfetch-mac.tar.gz"
+    url "https://github.com/furtidev/resfetch/releases/download/1.0.2/resfetch-mac.tar.gz"
     sha256 "f24118a8f6f82152308a6c2af359fd631551fc8fbfe380e30d889ce12868bd5e"
     version "1.0.2"
   
