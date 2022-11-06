6a4f3f95fe79123a309e4d0ce455bed5403bd89c
HitBlast <67194380+hitblast@users.noreply.github.com>
2022-11-06 17:53:13 +0600

    re-added article to desc

--- Formula/resfetch.rb
+++ Formula/resfetch.rb
@@ -1,5 +1,5 @@
 class Resfetch < Formula
-  desc "Fast and minimal alternative to neofetch"
+  desc "A fast and minimal alternative to neofetch"
   homepage "https://github.com/furtidev/resfetch"
   url "https://github.com/furtidev/resfetch/archive/refs/tags/1.0.2.tar.gz"
   sha256 "a04fa166a471dae6bf11be845d5e73df6210a5dc0ff5215db2d1067270d81902"
